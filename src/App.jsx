6285ef97f96534b49d2c6effa306f1cb1a519671
JoeTurpo <joeturpo@gmail.com>
2025-06-27 17:48:58 -0500

    style

--- src/App.jsx
+++ src/App.jsx
@@ -27,9 +27,11 @@ import PassengerHelpPortal from "./pages/passenger/PassengerHelpPortal";
 /* driver   ------------------------------------------------------------ */
 import DriverHomePage from "./pages/driver/DriverHomePage";
 import DriverProfilePage from "./pages/driver/DriverProfilePage";
+
 import DriverHelpPortal from "./pages/driver/DriverHelpPortal";
 import DriverHistoryPage from "./pages/driver/DriverHistoryPage";
-import DriverTripCompletedDetails from "./pages/driver/DriverTripCompletedDetails"; // Placeholder, puedes cambiarlo más tarde
+
+import DriverTripCompletedDetails from "./pages/driver/DriverTripCompletedDetails";
 /* (cuando tengas Home + Profile del conductor los importas igual)      */
 
 import PrivateRoute from "./auth/PrivateRoute";
